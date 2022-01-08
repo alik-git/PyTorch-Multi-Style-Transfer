d87e74d7cbb886edbe7b19a59db13ce03e8e04e0
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-01-08 14:33:03 -0500

    update

--- experiments/mandlebrot_demo.py
+++ experiments/mandlebrot_demo.py
@@ -2,7 +2,10 @@ import os
 import cv2
 import numpy as np
 import torch
+import pathlib
 from torch.autograd import Variable
+from datetime import datetime
+
 
 from net import Net
 from option import Options
@@ -30,67 +33,67 @@ def run_demo(args, mirror=False):
     width = int(4.0/3*args.demo_size)
     swidth = int(width/4)
     sheight = int(height/4)
-    if args.record:
-        fourcc = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
-        out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (2*width, height))
-    # cam = cv2.VideoCapture(0)
-    cam = cv2.VideoCapture('images/content/mzoom.mp4')
-    cam.set(3, width)
-    cam.set(4, height)
-    key = 0
-    idx = 0
-    while True:
-        # read frame
-        idx += 1
-        ret_val, styled_frame = cam.read()
-        if mirror:
-            styled_frame = cv2.flip(styled_frame, 1)
-        content_image = styled_frame.copy()
-        styled_frame = np.array(styled_frame).transpose(2, 0, 1)
-        # changing style
-        if idx % 20 == 1:
-            style_v = style_loader.get(int(idx/20))
-            style_v = Variable(style_v.data)
-            style_model.setTarget(style_v)
-
-        styled_frame = torch.from_numpy(styled_frame).unsqueeze(0).float()
-        if args.cuda:
-            styled_frame = styled_frame.cuda()
-
-        styled_frame = Variable(styled_frame)
-        styled_frame = style_model(styled_frame)
-        # cv2.imshow("Image", img)
-
-        if args.cuda:
-            style_image = style_v.cpu().data[0].numpy()
-            styled_frame = styled_frame.cpu().clamp(0, 255).data[0].numpy()
-        else:
-            style_image = style_v.data.numpy()
-            styled_frame = styled_frame.clamp(0, 255).data[0].numpy()
-        style_image = np.squeeze(style_image)
-        styled_frame = styled_frame.transpose(1, 2, 0).astype('uint8')
-        style_image = style_image.transpose(1, 2, 0).astype('uint8')
-        
-
-        # display
-        style_image = cv2.resize(style_image, (swidth, sheight),
-                          interpolation=cv2.INTER_CUBIC)
-        
-        content_image[0:sheight, 0:swidth, :] = style_image
-        cv2.imshow("Left Image", style_image)
-        # img = np.concatenate((cimg, img), axis=1)
-        # img = np.concatenate((cimg, img), axis=1)
-        cv2.imshow('MSG Demo', styled_frame)
-        # cv2.imwrite('stylized/%i.jpg'%idx,img)
-        key = cv2.waitKey(1)
+    for style_idx, style_option in enumerate(style_loader.files):
+        style_v = style_loader.get(int(style_idx))
         if args.record:
-            out.write(styled_frame)
-        if key == 27:
-            break
-    cam.release()
-    if args.record:
-        out.release()
-    cv2.destroyAllWindows()
+            fourcc = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
+            pathlib.Path('/outputs/').mkdir(parents=True, exist_ok=True)
+            out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (2*width, height))
+        # cam = cv2.VideoCapture(0)
+        cam = cv2.VideoCapture('images/content/mzoom.mp4')
+        cam.set(3, width)
+        cam.set(4, height)
+        key = 0
+        idx = 0
+        while True:
+            # read frame
+            idx += 1
+            ret_val, styled_frame = cam.read()
+            if mirror:
+                styled_frame = cv2.flip(styled_frame, 1)
+            content_image = styled_frame.copy()
+            styled_frame = np.array(styled_frame).transpose(2, 0, 1)
+            # changing style
+            # if idx % 20 == 1:
+            #     style_v = style_loader.get(int(idx/20))
+            #     style_v = Variable(style_v.data)
+            #     style_model.setTarget(style_v)
+
+            styled_frame = torch.from_numpy(styled_frame).unsqueeze(0).float()
+            if args.cuda:
+                styled_frame = styled_frame.cuda()
+
+            styled_frame = Variable(styled_frame)
+            styled_frame = style_model(styled_frame)
+            # cv2.imshow("Image", img)
+
+            if args.cuda:
+                style_image = style_v.cpu().data[0].numpy()
+                styled_frame = styled_frame.cpu().clamp(0, 255).data[0].numpy()
+            else:
+                style_image = style_v.data.numpy()
+                styled_frame = styled_frame.clamp(0, 255).data[0].numpy()
+            style_image = np.squeeze(style_image)
+            styled_frame = styled_frame.transpose(1, 2, 0).astype('uint8')
+            style_image = style_image.transpose(1, 2, 0).astype('uint8')
+            
+
+            # display
+            style_image = cv2.resize(style_image, (swidth, sheight),
+                            interpolation=cv2.INTER_CUBIC)
+            
+            content_image[0:sheight, 0:swidth, :] = style_image
+            cv2.imshow("Left Image", style_image)
+            cv2.imshow('MSG Demo', styled_frame)
+            key = cv2.waitKey(1)
+            if args.record:
+                out.write(styled_frame)
+            if key == 27:
+                break
+        cam.release()
+        if args.record:
+            out.release()
+        cv2.destroyAllWindows()
 
 
 def main():
